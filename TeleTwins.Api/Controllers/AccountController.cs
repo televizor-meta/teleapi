2faa9a79a6d57bef8c21072b03f3e97ad79d341a
Serafim Prozorov <serafim@prozorov.me>
2021-12-03 20:22:51 +0300

    Auth experiments

--- TeleTwins.Api/Controllers/AccountController.cs
+++ TeleTwins.Api/Controllers/AccountController.cs
@@ -10,35 +10,54 @@ namespace Rostelecom.DigitalProfile.Api.Controllers;
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
-    [ValidateModel]
-    [HttpPost("signin")]
-    [Consumes("application/json")]
-    [Produces("application/json")]
-    [ProducesResponseType(typeof(SignInResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(IDictionary<string, IEnumerable<string>>), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
-    [ProducesResponseType(typeof(object), StatusCodes.Status401Unauthorized)]
-    public async Task<IActionResult> SignIn(
-        [FromServices] ITvsLoginService service,
-        [FromBody] SignInRequest request,
-        CancellationToken cancellationToken)
+    private static Guid _id = Guid.NewGuid();
+    
+    // [ValidateModel]
+    // [HttpPost("signin")]
+    // [Consumes("application/json")]
+    // [Produces("application/json")]
+    // [ProducesResponseType(typeof(SignInResponse), StatusCodes.Status200OK)]
+    // [ProducesResponseType(typeof(IDictionary<string, IEnumerable<string>>), StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    // [ProducesResponseType(typeof(object), StatusCodes.Status401Unauthorized)]
+    // public async Task<IActionResult> SignIn(
+    //     [FromServices] ITvsLoginService service,
+    //     [FromBody] SignInRequest request,
+    //     CancellationToken cancellationToken)
+    // {
+    //     try
+    //     {
+    //         var response = await service.Login(request, cancellationToken);
+    //
+    //         if (response == null)
+    //             return Unauthorized();
+    //
+    //         return Ok(response);
+    //     }
+    //     catch (Exception x)
+    //     {
+    //         return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse {Message = x.Message});
+    //     }
+    // }
+
+    [HttpGet("login")]
+    public IActionResult Login() =>
+        Redirect("https://tvscp.tionix.ru/realms/master/protocol/openid-connect/auth/" +
+                 "?response_type=code&client_id=tvscp&scope=openid"
+                 $"&redirect_uri={Url.Action(nameof(SignInComplete), new { sid = _id })} ");
+    
+    [HttpGet("signinсomplete")]
+    public Task<IActionResult> SignInComplete(string sid)
     {
-        try
-        {
-            var response = await service.Login(request, cancellationToken);
-
-            if (response == null)
-                return Unauthorized();
-
-            return Ok(response);
-        }
-        catch (Exception x)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse {Message = x.Message});
-        }
+        return Task.FromResult(Ok() as IActionResult);
     }
 
-
+    [HttpGet]
+    public Task<IActionResult> GetAccessToken(string sid)
+    {
+        return null;
+    }
+    
     [Authorize]
     [HttpGet("check-it")]
     public Task<IActionResult> CheckIt() => Task.FromResult(Ok("It works!") as IActionResult);
